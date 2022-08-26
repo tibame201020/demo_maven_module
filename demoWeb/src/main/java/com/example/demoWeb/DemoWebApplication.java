8cba528500338b3809d296542b74272ef9c8414c
TSRI\Jeff <jeff.hsu@frog-jump.com>
2022-08-26 14:16:47 +0800

    can use on server web too

---
+++ demoWeb/src/main/java/com/example/demoWeb/DemoWebApplication.java
@@ -0,0 +1,18 @@
+package com.example.demoWeb;
+
+import org.dao.dao.impl.DaoImpl;
+import org.dao.dataSourceConfig.JdbcBeans;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@Import({DaoImpl.class, JdbcBeans.class})
+public class DemoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebApplication.class, args);
+	}
+
+}
---
+++ demoWeb/src/main/java/com/example/demoWeb/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.example.demoWeb.controller;
+
+
+import org.dao.dao.Dao;
+import org.dao.dataSourceConfig.Persistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private Dao dao;
+
+    @RequestMapping("test")
+    public List<Map<String, Object>> test() {
+        String sql = "select * from article";
+
+
+        return dao.query(sql, null, Persistence.DB1);
+    }
+}
